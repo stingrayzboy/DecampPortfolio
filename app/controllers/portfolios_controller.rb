a335d0d11cf86f34e296e9c5ec0001cb28516ba2
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-04 02:18:00 +0530

    Edit Method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,23 +3,40 @@ class PortfoliosController < ApplicationController
 		@portfolio_items=Portfolio.all	
 	end
 
-	def new
+	def edit
+		@portfolio=Portfolio.find(params[:id])
 	end
 
+	def new
+		@portfolio=Portfolio.new
+	end
 	
 	def create
-    @blog = Portfolio.new(params.require(:portfolio).permit(:title, :body,:thumb_image,:main_image,:subtitle))
+	    @blog = Portfolio.new(params.require(:portfolio).permit(:title, :body,:thumb_image,:main_image,:subtitle))
 
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
-      else
-        format.html { render :new }
-        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
-      end
+	    respond_to do |format|
+	      if @blog.save
+	        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+	        format.json { render :show, status: :created, location: @blog }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+	      end
+	    end  
     end
   
-
+    def update
+    	@portfolio=Portfolio.find(params[:id])
+    	respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :body,:thumb_image,:main_image,:subtitle))
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+    
+	
 	end
 end
