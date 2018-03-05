1594175d473b6cf38a54ce40d1bc8901e910599f
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-05 15:41:20 +0530

    Implemented Image dafault

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,11 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails, -> {where(subtitle:"Ruby on Rails")}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image||="http://via.placeholder.com/600x400"
+		self.thumb_image||="http://via.placeholder.com/350x200"
+	end
 end
