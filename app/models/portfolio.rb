a9b6bfd8d3984595c4fe79eb9bcdfd4b85bb0a6d
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-05 16:08:41 +0530

    Implemented cocerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title,:body,:main_image,:thumb_image
-
+	
 	def self.angular
 		where(subtitle:"Angular")
 	end
@@ -10,7 +11,8 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image||="http://via.placeholder.com/600x400"
-		self.thumb_image||="http://via.placeholder.com/350x200"
+	
+		self.main_image||=Placeholder.image_generator(600,400)
+		self.thumb_image||=Placeholder.image_generator(350,200)
 	end
 end
