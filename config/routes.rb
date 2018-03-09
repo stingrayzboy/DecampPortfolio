7442ecb35b48bb426ee33a00a9a5de678bbc5076
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-10 01:03:28 +0530

    Custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,path: "",path_names:{:sign_in=>"login",sign_out:"logout",sign_up:"register"}
+
+
   resources :portfolios ,except: [:show]
 
   get 'portfolio/:id', to:"portfolios#show", as: "show_portfolio"
