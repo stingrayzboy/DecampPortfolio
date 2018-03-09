44bd4f7dc049d7da590e25f0c9c628123d58fab2
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-10 01:38:46 +0530

    Added custom field to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_params, if: :devise_controller?
+  def configure_params
+  	devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  	devise_parameter_sanitizer.permit(:account_update,keys:[:name])
+
+  end
+
 end
