0173051471e5a3f1d73f7a91fa32aebaab7457a8
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-11 18:57:37 +0530

    Session Tracking

---
+++ app/controllers/concerns/session_setting.rb
@@ -0,0 +1,11 @@
+module SessionSetting
+	extend ActiveSupport::Concern
+	included do 
+		before_action :set_source
+	end
+	
+	def set_source
+  		session[:source]=params[:q] if params[:q]
+  	end
+
+end
