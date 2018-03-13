895de632b558ad90e94f2cf66ea3242836ce55fd
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-13 14:24:11 +0530

    Implemented helper for login logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def logged_in
+		if current_user.is_a?(User)
+        (link_to "Log out",destroy_user_session_path,method: :delete).html_safe
+    	else
+        "#{(link_to "Log in",new_user_session_path).html_safe}<br>#{(link_to "Sign Up!",new_user_registration_path).html_safe}".html_safe
+        
+    	end
+	end
 end
