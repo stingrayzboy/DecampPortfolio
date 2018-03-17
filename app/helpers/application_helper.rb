6b8cc7bab91be1234a8591f0d0116d9a97351b90
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-18 03:25:33 +0530

    Added Portfolio Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 		if !(current_user.is_a?(GuestUser))
         (link_to "Log out",destroy_user_session_path,method: :delete,class:style).html_safe
     	else
-        "#{(link_to "Log in",new_user_session_path,class:style).html_safe}#{(link_to "Sign Up!",new_user_registration_path,class:style).html_safe}".html_safe
+        "#{(link_to "Log in",new_user_session_path,class:style).html_safe}   #{(link_to "Sign Up!",new_user_registration_path,class:style).html_safe}".html_safe
         
     	end
 	end
