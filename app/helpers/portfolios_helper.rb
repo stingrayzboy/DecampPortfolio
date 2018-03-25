f3650ff93d971899f8a983423b94f21d8ab79bee
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-03-25 17:30:06 +0530

    Update file upload forms

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,19 @@
 module PortfoliosHelper
+
+
+def image_generator(height,width)
+	"http://via.placeholder.com/#{height}x#{width}"
+end
+
+def portfolio_img img,style
+	
+	if img.model.main_image? || img.model.thumb_image?
+		img
+	elsif style=="thumb"
+		image_generator(350,200)
+	elsif style=="main"
+		image_generator(600,400)		
+	end
+end
+
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,7 @@ class Portfolio < ApplicationRecord
 	has_many :technologies,dependent: :destroy
 	accepts_nested_attributes_for :technologies,
 									reject_if: lambda {|attrs| attrs["name"].blank?}
-	include Placeholder
-	validates_presence_of :title,:body,:main_image,:thumb_image
+	validates_presence_of :title,:body
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
@@ -15,13 +14,7 @@ class Portfolio < ApplicationRecord
 
 	scope :ruby_on_rails, -> {where(subtitle:"Ruby on Rails")}
 
-	after_initialize :set_defaults
-
-	def set_defaults
 	
-		self.main_image||=Placeholder.image_generator(600,400)
-		self.thumb_image||=Placeholder.image_generator(350,200)
-	end
 
 	def self.save_sexy(blog)
 		
