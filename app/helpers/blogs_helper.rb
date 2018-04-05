87e8233ff765edadd1d92ca7d0ea175fcfd726c7
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-04-05 23:23:39 +0530

    Implemented View helper for draft published and added Icons for edit delete and toggle

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,15 @@
 module BlogsHelper
 
-def gravatar_helper user
- image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}",width:60,class:"image-comment"	
-end
+	def gravatar_helper user
+	 image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}",width:60,class:"image-comment"	
+	end
+
+	def blog_status_color(blog)
+		if blog.draft?
+			"color:red;"
+		else
+			"color:green"
+		end
+	end
 
 end
